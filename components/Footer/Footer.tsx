16e7fe77fd44321594befbe9e1a902a62cccd7d8
Syllvvja02 <mikolajczykdrazeksylwia@gmail.com>
2023-07-04 13:46:06 +0200

    Footer icons

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,27 @@
-const Footer = () => {
+import { TbClockHour8 } from "react-icons/tb";
+import { MdDateRange } from "react-icons/md";
+
+let today = new Date();
+let date =
+  today.getDate() + "." + (today.getMonth() + 1) + "." + today.getFullYear();
+
+let actual_time = new Date();
+let time = today.getHours() + ":" + today.getMinutes();
+
+const Footer: React.FC = () => {
   return (
     <footer>
-      <div>Hour</div>
+      <div className="grid grid-cols-4">
+        <div className="">
+          <MdDateRange />
+        </div>
+        <div className="">{date}</div>
+
+        <div className="">
+          <TbClockHour8 />
+        </div>
+        <div className="">{time}</div>
+      </div>
     </footer>
   );
 };
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,12 @@
-const Header = () => {
+const Header: React.FC = () => {
   return (
     <header>
-      <div>MQNews</div>
-      <div>Kafelki</div>
-      <div>Lista</div>
-      <div>Button</div>
+      <div className="grid grid-cols-8 gap-2 m-10">
+        <div className="col-start-1 col-end-6">MQNews</div>
+        <div>Kafelki</div>
+        <div>Lista</div>
+        <div>Button</div>
+      </div>
     </header>
   );
 };
