442a04b1c259ca6fef59e83f065877730219b479
Syllvvja02 <mikolajczykdrazeksylwia@gmail.com>
2023-07-04 12:44:42 +0200

    Start Next App & create Footer and Header components

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div>Hour</div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Header/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <div>MQNews</div>
+      <div>Kafelki</div>
+      <div>Lista</div>
+      <div>Button</div>
+    </header>
+  );
+};
+
+export default Header;
