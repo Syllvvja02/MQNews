edccbecd1cd4467ea1137a6ba799f880403a8a36
Syllvvja02 <mikolajczykdrazeksylwia@gmail.com>
2023-07-10 14:11:55 +0200

    Sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/Navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Header />
+        <Navbar />
         {children}
         <Footer />
       </body>
---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,35 @@
+import styles from "./Navbar.module.css";
+import poland from "../../public/flags/poland.svg";
+import england from "../../public/flags/united-kingdom.svg";
+import france from "../../public/flags/france.svg";
+import germany from "../../public/flags/germany.svg";
+
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <nav className={`fixed w-20 ${styles.navbar}`}>
+      <p>Countries</p>
+      <ul className="flex p-0 m-0">
+        <li className="space-x-2">
+          <Image src={poland} width={10} height={10} alt="Poland flag" />
+          <p>Poland</p>
+        </li>
+        <li className="space-x-2">
+          <Image src={england} width={10} height={10} alt="England flag" />
+          <p>England</p>
+        </li>
+        <li className="space-x-2">
+          <Image src={france} width={10} height={10} alt="France flag" />
+          <p>France</p>
+        </li>
+        <li className="space-x-2">
+          <Image src={germany} width={10} height={10} alt="Germany flag" />
+          <p>Germany</p>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
